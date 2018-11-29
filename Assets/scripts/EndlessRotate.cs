8702b501d3a2cf62e3dd915c3d934a7c4b5839b0
huot14 <colehuot14@gmail.com>
2018-11-29 14:54:42 -0500

    Unity project setup

---
+++ Assets/scripts/EndlessRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessRotate : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+
+    Transform sprite;
+    Vector3 rotation = new Vector3(0.0f,0.0f,1.0f);
+
+	// Use this for initialization
+	void Start ()
+    {
+        sprite = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        sprite.Rotate(speed * rotation * Time.deltaTime);
+	}
+}
