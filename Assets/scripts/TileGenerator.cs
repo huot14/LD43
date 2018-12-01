c3c675a2d292be2e8ac4bcecc74610c55ed8cc60
huot14 <colehuot14@gmail.com>
2018-12-01 18:58:59 -0500

    Created a tile generator

---
+++ Assets/scripts/TileGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour {
+
+    // n*m grid of tiles
+    public int n;
+    public int m;
+
+    public float separationMultiplier;
+
+    public GameObject tilePrefab;
+
+
+	void Start ()
+    {
+        for (int z = 0; z < m; z++)
+        {
+            for (int x = 0; x < n; x++)
+            {
+                GameObject tile = GameObject.Instantiate(tilePrefab);
+                tile.transform.position = new Vector3(x * separationMultiplier, 0, z * separationMultiplier);
+            }
+        }
+
+	}
+}
