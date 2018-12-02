09fffc236d137ee86e4d9dd448ba2fcd3262f377
David Blajda <blajda@hotmail.com>
2018-12-02 22:09:34 +0000

    Allow custom scoring for each level

---
+++ Assets/scripts/level/alphaScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alphaScore : MonoBehaviour, ScoreCalculator {
+
+	public int score() {
+		if (Level.instance == null) {
+			return 0;
+		}
+		var level = Level.instance;
+
+		var total_prisoners = level.total_prisoners;
+		var killed_prisoners = level.killed_prisoners;
+
+		return total_prisoners - killed_prisoners;
+	}
+}
