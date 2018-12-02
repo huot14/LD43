c656a1543195cb4fd43e4a8435505c3bfbadb24a
huot14 <colehuot14@gmail.com>
2018-12-01 21:07:40 -0500

    Added tile layer to TileGenerator

--- Assets/scripts/TileGenerator.cs
+++ Assets/scripts/TileGenerator.cs
@@ -28,6 +28,7 @@ public class TileGenerator : MonoBehaviour {
 				Debug.Log ((z * columns) + x);
 				tile.transform.SetParent (parent.transform);
 				tile.transform.position = new Vector3(x * separationMultiplier, 0, z * separationMultiplier);
+                tile.layer = 8;
 			}
 		}
 
